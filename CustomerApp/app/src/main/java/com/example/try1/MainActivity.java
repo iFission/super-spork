6fad1d25d716a24cbddcfea65c44bbf05a88d90d
Jose Johnson <jose.johnson.jose@gmail.com>
2019-11-13 00:54:28 +0800

    feat:Not working :/

--- CustomerApp/app/src/main/java/com/example/try1/MainActivity.java
+++ CustomerApp/app/src/main/java/com/example/try1/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.try1;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -7,6 +8,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import com.google.firebase.database.*;
 
 import java.util.ArrayList;
 
@@ -19,10 +22,19 @@ public class MainActivity extends AppCompatActivity {
     String order2;
     String order3;
 
+    TextView ChickenChopTextView;
+
+    DatabaseReference mRootRef= FirebaseDatabase.getInstance().getReference();   //Gives you the root of the JSON tree
+//     Creates a location of menu underneath the roots, which can receive a value
+    DatabaseReference mfoodRef = mRootRef.child("Menu");
+    DatabaseReference mconditionRef = mRootRef.child("condition");
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ChickenChopTextView = (TextView) findViewById(R.id.ChickenChopTextView);
 
 
         Button cart = findViewById(R.id.Cart);
@@ -41,7 +53,6 @@ public class MainActivity extends AppCompatActivity {
         ChickenChopButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                TextView ChickenChopTextView = findViewById(R.id.ChickenChopTextView);
                 order1 = ChickenChopTextView.getText().toString();
 
             }
@@ -63,6 +74,35 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 TextView NuggetsTextView = findViewById(R.id.NuggetsTextView);
                 order3 = NuggetsTextView.getText().toString();
+
+            }
+        });
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        //Attach the eventListener to the condition child to see if there are any changes.
+        mfoodRef.addValueEventListener(new ValueEventListener() {
+
+            //Will run everytime there is an update to the condition value in the database
+            //So this will run when the .setValue function runs in the button onClickListener classes
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                ChickenChopTextView.setText("FUCKFUCK");
+
+//                ChickenChopTextView.setText(Long.toString(dataSnapshot.getChildrenCount()));
+//                DataSnapshot foodmenu1 = dataSnapshot.child("Menu");
+//                Menu menu1 = foodmenu1.getValue(Menu.class);             //Retrieves the value of condition in the database
+//                DataSnapshot foodmenu2 = dataSnapshot.child("Menu1");
+//                Menu menu2 = foodmenu2.getValue(Menu.class);
+//                ChickenChopTextView.setText(menu1.foodName);
+//                FriesTextView.setText(menu2.foodName);
+
+            }
+
+            // In case we run into any errors
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
 
             }
         });
