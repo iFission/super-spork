99bd2418e693ee4ca318887a56c1bc368cba0c31
Jose Johnson <jose.johnson.jose@gmail.com>
2019-11-13 13:09:48 +0800

    feat: merge try1 with ori

--- Firebase/app/src/main/java/com/example/myapplication/MainActivity.java
+++ Firebase/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -20,34 +20,38 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class MainActivity extends AppCompatActivity {
 
-    TextView mConditionTextview;
-    TextView mMenuTextview;
-    Button mButtonSunny;
-    Button mButtonFoggy;
+public class MainActivity extends AppCompatActivity {
+    //Debug TAG
+    private static final String TAG = "FirebaseApp";
+
+    public final static String ORDER1 = "ORDER1";
+    public final static String ORDER2 = "ORDER2";
+    public final static String ORDER3 = "ORDER3";
+
+    String order1;
+    String order2;
+    String order3;
+
+    TextView ChickenChopTextView;
 
     DatabaseReference mRootRef= FirebaseDatabase.getInstance().getReference();   //Gives you the root of the JSON tree
-    // Creates a location of condition underneath the roots, which can receive a value
-    DatabaseReference mconditionRef = mRootRef.child("condition");
+    //     Creates a location of menu underneath the roots, which can receive a value
     DatabaseReference mfoodRef = mRootRef.child("Menu");
-    DatabaseReference mlistRef = mRootRef.child("orderQueue");
+    DatabaseReference mconditionRef = mRootRef.child("condition");
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mConditionTextview=(TextView)findViewById(R.id.Textviewcondition);
-        mButtonSunny=(Button)findViewById(R.id.Sunnybutton);
-        mButtonFoggy=(Button)findViewById(R.id.Foggybutton);
-        mMenuTextview=(TextView)findViewById(R.id.MenuTextView);
-    }
+        ChickenChopTextView = (TextView) findViewById(R.id.ChickenChopTextView);
 
-    @Override
-    protected void onStart() {
-        super.onStart();
 
-        //Attach the eventListener to the condition child to see if there are any changes.
+
+
+        ChickenChopTextView.setText("Okaylo");
+
         mfoodRef.addValueEventListener(new ValueEventListener() {
 
             //Will run everytime there is an update to the condition value in the database
@@ -57,7 +61,7 @@ public class MainActivity extends AppCompatActivity {
                 DataSnapshot foodmenu1 = dataSnapshot.child("Menu");
                 Menu text1 = foodmenu1.getValue(Menu.class);             //Retrieves the value of condition in the database
                 //String foodName = text1.foodName;
-                mConditionTextview.setText(text1.foodName);
+                ChickenChopTextView.setText(text1.foodName);
 //                String blach;
 //                for (DataSnapshot child :
 //                        text) {
@@ -75,8 +79,7 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
-
-        mButtonSunny.setOnClickListener(new View.OnClickListener() {
+        ChickenChopTextView.setOnClickListener(new View.OnClickListener() {
 
             //Setting the value of the condition child to be the string
             @Override
@@ -84,22 +87,71 @@ public class MainActivity extends AppCompatActivity {
                 //mconditionRef.setValue("It works!!!!!");
                 Menu mexicanChiChop = new Menu("Mexican Chicken Chop", 4.50);
                 Menu AglioOlio = new Menu("Aglio Olio", 4.50);
+                mfoodRef.addValueEventListener(new ValueEventListener() {
+
+                    //Will run everytime there is an update to the condition value in the database
+                    //So this will run when the .setValue function runs in the button onClickListener classes
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        ChickenChopTextView.setText("text1.foodName");
+
+                        DataSnapshot foodmenu1 = dataSnapshot.child("Menu");
+                        Menu text1 = foodmenu1.getValue(Menu.class);             //Retrieves the value of condition in the database
+                        //String foodName = text1.foodName;
+                        ChickenChopTextView.setText(text1.foodName);
+//                String blach;
+//                for (DataSnapshot child :
+//                        text) {
+//                    blach = child.getValue().toString();
+//
+//
+//                }
+//                mConditionTextview.setText(blach);
+
+                    }
+
+                    // In case we run into any errors
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
             }
         });
-
-        mButtonFoggy.setOnClickListener(new View.OnClickListener() {
-
-            //Setting the value of the condition child to be the string
-            @Override
-            public void onClick(View v) {
-                mconditionRef.setValue("It really does!!! All synced!!");
-                Map<String, String> orderQueue = new HashMap<String, String>();
-                orderQueue.put(Integer.toString(100),"chicken chop");
-                orderQueue.put(Integer.toString(101),"rice");
-                orderQueue.put(Integer.toString(102),"chicken chop");
-                mlistRef.child("items").setValue(orderQueue);
-            }
-        });
-
     }
+//    @Override
+//    protected void onStart() {
+//        super.onStart();
+////        ChickenChopTextView.setText("Okaylo");
+//        //Attach the eventListener to the condition child to see if there are any changes.
+//        mfoodRef.addValueEventListener(new ValueEventListener() {
+//
+//            //Will run everytime there is an update to the condition value in the database
+//            //So this will run when the .setValue function runs in the button onClickListener classes
+//            @Override
+//            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+//                Log.i(TAG,"onDataChange : " + dataSnapshot);
+//                DataSnapshot foodmenu1 = dataSnapshot.child("Menu1");
+//                Menu text1 = foodmenu1.getValue(Menu.class);
+//                ChickenChopTextView.setText(text1.foodName);
+//
+////                ChickenChopTextView.setText(Long.toString(dataSnapshot.getChildrenCount()));
+////                DataSnapshot foodmenu1 = dataSnapshot.child("Menu");
+////                Menu menu1 = foodmenu1.getValue(Menu.class);             //Retrieves the value of condition in the database
+////                DataSnapshot foodmenu2 = dataSnapshot.child("Menu1");
+////                Menu menu2 = foodmenu2.getValue(Menu.class);
+////                ChickenChopTextView.setText(menu1.foodName);
+////                FriesTextView.setText(menu2.foodName);
+//
+//            }
+//
+//            // In case we run into any errors
+//            @Override
+//            public void onCancelled(@NonNull DatabaseError databaseError) {
+//
+//            }
+//        });
+//    }
 }
+
+
