e302df46f6dc8567d24c7861db1d02077f4ff7d7
Jose Johnson <jose.johnson.jose@gmail.com>
2019-11-26 15:35:22 +0800

    feat: integrate firebase to VendorAttempt2

--- VendorAttempt2/app/src/main/java/com/example/vendorattempt2/MainActivity.java
+++ VendorAttempt2/app/src/main/java/com/example/vendorattempt2/MainActivity.java
@@ -1,14 +1,63 @@
 package com.example.vendorattempt2;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView trialtextview;
+    TextView trialtextview2;
+
+    DatabaseReference mRootRef= FirebaseDatabase.getInstance().getReference();   //Gives you the root of the JSON tree
+    //     Creates a location of menu underneath the roots, which can receive a value
+    DatabaseReference mfoodRef = mRootRef.child("Menu");
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        trialtextview = (TextView) findViewById(R.id.textview1);
+        trialtextview2 = (TextView) findViewById(R.id.textview2);
+        trialtextview.setText("hey there");
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mfoodRef.addValueEventListener(new ValueEventListener() {
+
+            //Will run everytime there is an update to the condition value in the database
+            //So this will run when the .setValue function runs in the button onClickListener classes
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                DataSnapshot foodmenu1 = dataSnapshot.child("Menu");
+                Menu text1 = foodmenu1.getValue(Menu.class);             //Retrieves the value of menu in the database
+                trialtextview.setText(text1.foodName);
+
+                DataSnapshot foodmenu2 = dataSnapshot.child("Menu1");
+                Menu text2 = foodmenu2.getValue(Menu.class);
+                trialtextview2.setText(text2.foodName);
+
+
+            }
+
+            // In case we run into any errors
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 }
